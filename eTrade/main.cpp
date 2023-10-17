3000e2168533a13fdbe7a7452570ef102ff26c3e
Cemal Demir <CMLDMR@users.noreply.github.com>
2023-10-17 11:17:23 +0300

    eTradeSystem

--- eTrade/main.cpp
+++ eTrade/main.cpp
@@ -1,11 +1,8 @@
-#include <QCoreApplication>
-
 #include "MainApplication.h"
 
 
 int main(int argc, char *argv[])
 {
-
     return Wt::WRun(argc,argv,[](const Wt::WEnvironment &env){
         return std::make_unique<MainApplication>(env);
     });
